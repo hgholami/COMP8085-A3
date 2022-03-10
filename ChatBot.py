974f3762e1d108cae8eb7eeb2f5a8c173e3fa6e5
Houman Gholami <35938542+hgholami@users.noreply.github.com>
2022-03-10 14:18:17 -0800

    Initial files added

---
+++ ChatBot.py
@@ -0,0 +1,15 @@
+import sys
+
+args = sys.argv
+num_of_args = 1
+
+def main():
+    if(len(args) != num_of_args):
+        print("Expected number of arguments is ", num_of_args, "...")
+        return
+    
+    #arg1 = args[1]
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,2 @@
+Downloaded from:
+https://www.kaggle.com/itachi9604/disease-symptom-description-dataset
